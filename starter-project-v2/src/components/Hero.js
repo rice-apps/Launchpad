2eda29672c22cbd828f6d120367caf500bd97f6b
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-11-05 19:58:09 -0600

    Hero image updates on interval

--- starter-project-v2/src/components/Hero.js
+++ starter-project-v2/src/components/Hero.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import useInterval from '@use-it/interval';
+import React, { useState } from 'react';
+
+const allHeroURL = [
+  "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+  "https://images.unsplash.com/photo-1604584741084-6bdd6792213d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+  "https://images.unsplash.com/photo-1525625293386-3f8f99389edd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1849&q=80",
+  "https://images.unsplash.com/photo-1565967511849-76a60a516170?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80",
+  "https://images.unsplash.com/photo-1505662695181-d4b60363d2a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1815&q=80"
+]
 
 function Hero() {
-  
-  const heroURL = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/81b63702-a53f-4dc2-9bd4-31ea64536391/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201010%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201010T195740Z&X-Amz-Expires=86400&X-Amz-Signature=ac8b0beda7438185252bf37fe8bbef19bb538c3f5cd906b77fc01bf1b7ad919c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22";
+  const [getHeroURLIndex, setHeroURLIndex] = useState(0);
 
+  useInterval(() => {
+    if (getHeroURLIndex < allHeroURL.length) {
+      setHeroURLIndex(getHeroURLIndex + 1);
+    } else {
+      // Reset to 0 once we've run through the list
+      setHeroURLIndex(0);
+    }
+  }, 2000);
+  
   return (
     <div className="heroImageContainer">
-      <img className="heroImage" src={heroURL} />
+      <img className="heroImage" src={allHeroURL[getHeroURLIndex]} />
     </div>
   );
 }
