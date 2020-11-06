3c5e0a3fc2204e51cd3f17a325017ab183424820
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-11-05 20:16:32 -0600

    Dynamic content boxes from baconipsum

--- starter-project-v2/src/components/DescriptionBoxes.js
+++ starter-project-v2/src/components/DescriptionBoxes.js
@@ -1,4 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const initialContentBoxes = [
+	{
+		title: "OSA.",
+		description: ""
+		// 	"Our premier summer internship for underclassmen. mentorship, impactful projects, and more can be found in the open source accelerator: the first program of its kind in the united states.",
+	},
+	{
+		title: "Launchpad.",
+		description: ""
+		// 	"Our premier fall program for new web developers. launchpad is where impact begins and learning ends: the first program of its kind in the united states.",
+	},
+	{
+		title: "Social Good.",
+		description: ""
+		// 	"This is the heart of riceapps. if there is no impact, there is no project. we’re always looking for new opportunities to create good through our digital solutions, so please reach out if your organization needs dedicated problem-solvers to tackle an issue!",
+	},
+];
 
 const DescriptionBox = ({ title, description }) => {
 	return (
@@ -10,15 +28,23 @@ const DescriptionBox = ({ title, description }) => {
 };
 
 function DescriptionBoxes() {
-	const contentBoxes = [
-		{ title: "OSA.", description: "Our premier summer internship for underclassmen. mentorship, impactful projects, and more can be found in the open source accelerator: the first program of its kind in the united states." },
-		{ title: "Launchpad.", description: "Our premier fall program for new web developers. launchpad is where impact begins and learning ends: the first program of its kind in the united states." },
-		{ title: "Social Good.", description: "This is the heart of riceapps. if there is no impact, there is no project. we’re always looking for new opportunities to create good through our digital solutions, so please reach out if your organization needs dedicated problem-solvers to tackle an issue!" },
-	];
+	const [getContentBoxes, setContentBoxes] = useState(initialContentBoxes);
+
+	useEffect(() => {
+		const getDescriptions = async () => {
+			const descriptionRequest = await fetch(
+				"https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1"
+			);
+			const descriptionText = await descriptionRequest.json();
+			const newDescriptionBoxes = initialContentBoxes.map((box, idx) => ({ title: box.title, description: descriptionText[idx] }) );
+			setContentBoxes(newDescriptionBoxes);
+		};
+		getDescriptions();
+	}, []);
 
 	return (
 		<div className="descriptionBoxContainer">
-			{contentBoxes.map((contentBox) => (
+			{getContentBoxes.map((contentBox) => (
 				<DescriptionBox
 					title={contentBox.title}
 					description={contentBox.description}
